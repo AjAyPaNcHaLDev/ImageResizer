29ed472241cf5fd5fd3af4b73a4f877fe22a5fc1
AjAyPaNcHaLDev <ap8570091@gmail.com>
2022-03-19 15:23:45 +0530

    layout update

--- app/src/main/java/ajay/developer/ImageResizer/MainActivity.java
+++ app/src/main/java/ajay/developer/ImageResizer/MainActivity.java
@@ -3,25 +3,26 @@ package ajay.developer.ImageResizer;
 import androidx.appcompat.app.AppCompatActivity;
 
 
+import android.content.Context;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 
 
 import ajay.developer.Params.parms;
-
-import ajay.developer.backEnd.backEnd;
+import ajay.developer.backEnd.Permission;
 
 
 public class MainActivity extends AppCompatActivity {
 
+
+    Context context=this;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        parms.setFiles(findViewById(R.id.files));
-        parms.setCamera(findViewById(R.id.camera));
-
-new backEnd(MainActivity.this,this);
+        parms.setFiles(findViewById(R.id.galleryBtn));
+        parms.setCamera(findViewById(R.id.cameraBtn));
 
 
     }
@@ -31,5 +32,13 @@ new backEnd(MainActivity.this,this);
         super.onResume();
 
 Log.e("TAGA","resume app");
+    }
+
+    public void cameraActionBtn(View view) {
+        new Permission(context, MainActivity.this,"camera");
+    }
+
+    public void galleryActionBtn(View view) {
+        new Permission(context, MainActivity.this,"storage");
     }
 }
